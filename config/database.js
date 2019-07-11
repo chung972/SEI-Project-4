4ee0b672ca2f6a92e7112bb59457df0ae0bc91f3
Albert Chung <chung972@gmail.com>
2019-07-11 12:43:12 -0500

    adds catch function for promise rejection

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true });
-
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true })
+.catch(err => console.log('A Mongoose Error Was Thrown: ', err));
 
 db.once('connected', () => {
   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
 
-db.on('error', (err) => console.log(err));
