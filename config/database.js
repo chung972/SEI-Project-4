dd9fd3dab1ea5792d3799916f2e9ba16ff522a87
Albert Chung <chung972@gmail.com>
2019-07-11 12:30:08 -0500

    triggers rewrite

--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ const db = mongoose.connection;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true });
 
-
 db.once('connected', () => {
   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
