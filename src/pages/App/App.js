e699a6b3e016988a183d66dbb83397ef86e940bf
Albert Chung <chung972@gmail.com>
2019-07-12 14:42:02 -0500

    imports MoviePage and NavBar; adds NavBar above Switch; changes spacing; adds Route for /movie

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -6,6 +6,8 @@ import tokenService from "../../utils/tokenService";
 import HomePage from "../../pages/HomePage/HomePage";
 import SignupPage from "../../pages/SignupPage/SignupPage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
+import MoviePage from "../../pages/MoviePage/MoviePage";
+import NavBar from "../../components/NavBar/NavBar";
 
 class App extends Component {
   constructor() {
@@ -17,13 +19,25 @@ class App extends Component {
   }
 
   handleSignupOrLogin = () => {
-    this.setState({user: userService.getUser()});
+    this.setState({ user: userService.getUser() });
   }
 
   handleLogout = () => {
     userService.logout();
     this.setState({ user: null });
   }
+
+  displayMsg({ username, message, timestamp }) {
+    // var node = document.createElement("li");
+    // var msbItm = document.createTextNode(`[${timestamp}] ${username} says: ${message}`);
+    // node.appendChild(msbItm);
+    // msgBoard.appendChild(node);
+  }
+
+
+
+
+
 
   render() {
     return (
@@ -32,25 +46,33 @@ class App extends Component {
         <header>
           <h1>ReelTalk</h1>
         </header>
+        <NavBar
+          user={this.state.user}
+          handleLogout={this.handleLogout}
+        />
         <Switch>
           <Route exact path="/" render={() =>
             <HomePage
-              user={this.state.user}
-              handleLogout={this.handleLogout}
             />
-          }/>
-          <Route exact path="/signup" render={({history}) =>
-            <SignupPage 
+          } />
+          <Route exact path="/signup" render={({ history }) =>
+            <SignupPage
               history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
-          }/>
-          <Route exact path='/login' render={({ history }) => 
+          } />
+          <Route exact path="/login" render={({ history }) =>
             <LoginPage
               history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
-          }/>
+          } />
+          <Route exact path="/movie" render={() =>
+            <MoviePage
+              user={this.state.user}
+              handleLogout={this.handleLogout}
+            />
+          } />
         </Switch>
 
       </div>
