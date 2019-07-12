66505f01e1363cb90a3dd6bd50d03488f6509454
Albert Chung <chung972@gmail.com>
2019-07-12 14:37:44 -0500

    scaffolds ChatBoard component

---
+++ src/components/ChatBoard/ChatBoard.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class ChatBoard extends Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+            <h1>chatboard</h1>
+        )
+    }
+}
+
+export default ChatBoard;
