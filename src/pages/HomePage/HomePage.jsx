8da21e3669fa0c292d21160f201a92e2fa363a72
Albert Chung <chung972@gmail.com>
2019-07-16 17:22:29 -0500

    destructures props, passes props to SearchForm component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import SearchForm from "../../components/SearchForm/SearchForm";
 
-const HomePage = (props) => {
+const HomePage = ({match, history, idList, asyncAddId, chatBoards}) => {
     return (
         <div>
             <h1>Home</h1>
-            <SearchForm />
+            <SearchForm 
+                match={match}
+                history={history}
+                idList={idList}
+                asyncAddId={asyncAddId}
+                chatBoards={chatBoards}
+            />
         </div>
     );
 }
