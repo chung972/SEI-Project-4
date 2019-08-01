db56ad047337c37ba53e5681587f57c2a84874ba
Albert Chung <chung972@gmail.com>
2019-07-31 22:52:41 -0500

    adds comments

--- controllers/chatboards.js
+++ controllers/chatboards.js
@@ -20,6 +20,8 @@ function create(req, res){
     const chatboard = new Chatboard(req.body);
     try{
         chatboard.save();
+        // when you create the room (by saving to mongo), we will, at the same time,
+        // also want to create the chatkit room as well
     } catch(err){
         res.status(400).json(err);
     }
